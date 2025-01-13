d64fc5bae1f4584f439c3ba77ef3a7095858ffdb
anilyavas <anil.yavas99@gmail.com>
2025-01-13 14:00:31 +0300

    clerk setup

---
+++ app/(auth)/onboarding.tsx
@@ -0,0 +1,9 @@
+import { View, Text } from 'react-native';
+
+export default function OnboardingPage() {
+  return (
+    <View className="flex-1">
+      <Text className="text-2xl font-bold">Onboarding</Text>
+    </View>
+  );
+}
