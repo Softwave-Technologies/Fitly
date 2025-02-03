453c1e5919d0422c75f1de0b9f052850793ff85f
anilyavas <anil.yavas99@gmail.com>
2025-02-03 12:11:46 +0300

    Zustand setup and cleaning home screen features

--- app/(home)/index.tsx
+++ app/(home)/index.tsx
@@ -3,21 +3,11 @@ import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
 import { View, Text, SafeAreaView, Image } from 'react-native';
 
-const motivationalQuotes = [
-  'Push yourself, because no one else will do it for you.',
-  "Don't stop when you're tired. Stop when you're done.",
-  'Your only limit is you. Keep going!',
-  'The secret of getting ahead is getting started.',
-];
-
 export default function HomePage() {
   const { user } = useUser();
   const [workout, setWorkout] = useState(null);
 
-  //random quote for user
-  const quote = motivationalQuotes[Math.floor(Math.random() * motivationalQuotes.length)];
-
-  const fetchWorkuout = async () => {};
+  const createWorkuout = async () => {};
 
   return (
     <SafeAreaView className="flex-1 bg-black">
@@ -26,9 +16,6 @@ export default function HomePage() {
         {user?.imageUrl && (
           <Image source={{ uri: user.imageUrl }} className="h-16 w-16 rounded-full" />
         )}
-      </View>
-      <View className="p-4">
-        <Text className="text-center text-xl font-bold text-green-500">"{quote}"</Text>
       </View>
       <StatusBar style="light" />
     </SafeAreaView>
