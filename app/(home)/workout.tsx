37cd6823d01358b8f778c0f86ff6fae41b3432cd
anilyavas <anil.yavas99@gmail.com>
2025-02-25 15:52:17 +0300

    Prepared workouts for app and flashlist installation

--- app/(home)/workout.tsx
+++ app/(home)/workout.tsx
@@ -1,4 +1,9 @@
+import { FlashList } from '@shopify/flash-list';
 import { View, Text, SafeAreaView } from 'react-native';
+
+import preWorkouts from '../../assets/data/preWorkouts.json';
+
+import WorkoutsList from '~/components/WorkoutsList';
 
 export default function WorkoutPage() {
   return (
@@ -6,6 +11,13 @@ export default function WorkoutPage() {
       <SafeAreaView>
         <View className="border-b-hairline border-gray-300 p-6">
           <Text className="text-2xl font-bold text-green-500">Workout</Text>
+        </View>
+        <View className="p-4">
+          <Text className="text-lg font-semibold text-white">Our Workouts</Text>
+          <FlashList
+            data={preWorkouts}
+            renderItem={({ item }) => <WorkoutsList workout={item} />}
+          />
         </View>
       </SafeAreaView>
     </View>
