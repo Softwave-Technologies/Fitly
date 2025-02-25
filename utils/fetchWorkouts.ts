ef1b32791cc17a14e1b8a63a995363c185257ba7
anilyavas <anil.yavas99@gmail.com>
2025-02-25 15:23:54 +0300

    Delete separate function

--- utils/fetchWorkouts.ts
+++ utils/fetchWorkouts.ts
@@ -1,7 +1,14 @@
-import { supabase } from './/supabase';
+import { useWorkoutStore } from '../store/useWorkoutStore';
+import { supabase } from '../utils/supabase';
 
 export const fetchWorkouts = async () => {
   const { data, error } = await supabase.from('workouts').select('*');
-  if (error) console.error('Error fetching workouts:', error);
-  return data;
+
+  if (error) {
+    console.error('Error fetching workouts:', error);
+    return;
+  }
+
+  // Update Zustand state
+  useWorkoutStore.setState({ workouts: data || [] });
 };
