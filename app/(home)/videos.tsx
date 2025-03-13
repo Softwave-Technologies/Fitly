ea8b8f1910578c4c4d4792160e725b33f4b2cb6e
anilyavas <anil.yavas99@gmail.com>
2025-03-13 17:27:56 +0300

    Added and removed identical View and Text elements in Videos component

--- app/(home)/videos.tsx
+++ app/(home)/videos.tsx
@@ -8,10 +8,10 @@ export default function Videos() {
   return (
     <View className="flex-1 bg-gray-900">
       <SafeAreaView className="flex-1">
+        <View className="border-b-hairline border-gray-300 p-6">
+          <Text className="text-2xl font-bold text-green-500">Exercise Videos</Text>
+        </View>
         <ScrollView showsVerticalScrollIndicator={false}>
-          <View className="border-b-hairline border-gray-300 p-6">
-            <Text className="text-2xl font-bold text-green-500">Exercise Videos</Text>
-          </View>
           <View className="m-2 p-2">
             <FlashList
               data={exerciseVideos}
